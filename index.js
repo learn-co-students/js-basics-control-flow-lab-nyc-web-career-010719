317b06d3053b7d27da4a67f49072925475fe9e52
mistermur <brian.j.murillo@gmail.com>
2019-02-17 15:40:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code in this file!
+function scuberGreetingForFeet(num)
+{
+  if (num >2500)
+  {
+    return 'No can do.'
+  }
+  return num <=400 ? 'This one is on me!':'I will gladly take your thirty bucks.'
+};
+
+function ternaryCheckCity(city)
+{
+  return city == 'NYC' ? 'Ok, sounds good.':'No go.'
+};
+
+function switchOnCharmFromTip(tip)
+{
+  switch (tip){
+    case 'generous': return 'Thank you so much.'
+    case 'not as generous': return 'Thank you.'
+    default: return 'Bye.'
+  }
+};
